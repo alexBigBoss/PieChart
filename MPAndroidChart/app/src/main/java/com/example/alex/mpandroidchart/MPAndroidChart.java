014f85f857478f2ae8d92e3302064c28d587df97
alexBigBoss <armando.sepulvedacrz@gmail.com>
2015-04-10 21:19:07 -0500

    More Colors

--- MPAndroidChart/app/src/main/java/com/example/alex/mpandroidchart/MPAndroidChart.java
+++ MPAndroidChart/app/src/main/java/com/example/alex/mpandroidchart/MPAndroidChart.java
@@ -9,6 +9,7 @@ import com.github.mikephil.charting.components.Legend.LegendPosition;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -19,7 +20,6 @@ public class MPAndroidChart {
 
     public void PiePlot(PieChart pieChart) {
 
-        int[] colors = {Color.BLUE, Color.YELLOW};
         Context context = pieChart.getContext();
         ArrayList<Entry> entries = new ArrayList<>();
         ArrayList<String> labels = new ArrayList<>();
@@ -30,9 +30,20 @@ public class MPAndroidChart {
 
         // Set the basic information of the graph
         entries.add(new Entry((float) 20.0, 0));
-        entries.add(new Entry((float) 30.0, 1));
+        entries.add(new Entry((float) 12.0, 1));
+        entries.add(new Entry((float) 25.0, 2));
+        entries.add(new Entry((float) 45.0, 3));
+        entries.add(new Entry((float) 9.0, 4));
+        entries.add(new Entry((float) 32.0, 5));
         labels.add("January");
         labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        // Set different colors
+        ArrayList<Integer> colors = setColors();
 
         // Set the Pie Chart properties
         dataSet = new PieDataSet(entries, "Months");
@@ -63,5 +74,29 @@ public class MPAndroidChart {
         l.setTextSize(18f);
 
         pieChart.addView(chart);
+    }
+
+    private ArrayList<Integer> setColors() {
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        return colors;
     }
 }
